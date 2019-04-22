1fcf5492cc1a6daf19424b9a5682477e5ff9485b
leuat <nicolaas.groeneboom@gmail.com>
2019-04-22 13:46:29 +0200

    Forgot main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <source/lglwrap.h>
+#include <source/lxm.h>
+
+#include <source/Scene1.h>
+
+int main( void )
+{
+    // Initialise GLFW
+    LXM xm;
+    xm.InitMusic();
+    LGLWrap app(&xm);
+    app.m_scenes.push_back(new Scene1(2000));
+    app.Init(600,400);
+
+    return app.Exec();
+}
